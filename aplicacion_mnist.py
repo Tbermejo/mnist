6c2d2a835845ef3d8f99f2c58b24aeac79089841
Tbermejo <lbermejo@unal.edu.co>
2025-02-03 20:50:46 -0500

    Update aplicacion_mnist.py

--- aplicacion_mnist.py
+++ aplicacion_mnist.py
@@ -6,6 +6,12 @@ import numpy as np
 import gzip
 import pickle
 
+[theme]
+primaryColor = "#0000FF"
+backgroundColor = "#FFFFFF"
+secondaryBackgroundColor = "#F0F0F0"
+textColor = "#000000"
+
 def preprocess_image(image):
     image = image.convert('L')  # Convertir a escala de grises
     image = image.resize((28, 28))
@@ -23,18 +29,17 @@ def main():
     st.set_page_config(page_title="Clasificación MNIST", layout="wide")
     st.title("🖼️ Clasificación de imágenes MNIST")
     st.write("""
-    ### Selección del Mejor Modelo Mediante Búsqueda de Hiperparámetros en SVM
+    ### Selección del Mejor Modelo Mediante Búsqueda de Hiperparámetros en diferentes métodos de clasificación.
 
-    En este análisis, se implementó un proceso sistemático para encontrar el mejor modelo de clasificación utilizando **Máquinas de Soporte Vectorial (SVM)**. Para ello, se realizó una búsqueda exhaustiva de hiperparámetros mediante la técnica de **Grid Search con validación cruzada**, con el objetivo de optimizar la precisión del modelo y mejorar su capacidad de generalización.
+    En este análisis, se implementó un proceso iterativo para encontrar el mejor modelo de clasificación utilizando el Clasificador Naive Bayes (`GaussianNB`)**, Árboles de decisión (`DecisionTreeClassifier`)**, **Máquinas de Soporte Vectorial (SVM)**, entre otros. Para ello, se realizó una búsqueda de hiperparámetros mediante la técnica de **Grid Search con validación cruzada**, con el objetivo de optimizar la precisión del modelo y mejorar su capacidad de generalización.
 
     El procedimiento inició con la definición de un conjunto de hiperparámetros a evaluar, los cuales incluyen el **parámetro de penalización (`C`)**, el **tipo de función de núcleo (`kernel`)** y el **parámetro de control de la variabilidad (`gamma`)**. Estos valores fueron explorados dentro de un espacio de búsqueda predefinido que abarcó múltiples configuraciones.
 
-    Posteriormente, se aplicó **GridSearchCV**, una herramienta de `scikit-learn` que permite probar todas las combinaciones posibles de hiperparámetros mediante validación cruzada. Esto aseguró que el modelo no solo se ajustara correctamente a los datos de entrenamiento, sino que también tuviera un buen desempeño en datos no vistos.
+    Posteriormente, se aplicó **GridSearchCV**, una herramienta de `scikit-learn` que permite probar todas las combinaciones posibles de hiperparámetros mediante validación cruzada. Esto aseguró que el modelo no solo se ajustara correctamente a los datos de entrenamiento, sino que también tuviera un buen desempeño en datos de prueba.
 
-    Una vez identificado el mejor conjunto de hiperparámetros, el modelo óptimo fue entrenado con la totalidad de los datos de entrenamiento y evaluado sobre el conjunto de prueba. Para medir su desempeño, se calcularon métricas clave como la **precisión (accuracy)**, además de visualizar su comportamiento mediante una **matriz de confusión y un reporte de clasificación**.
-
-    Este enfoque permitió seleccionar el modelo más adecuado para la tarea de clasificación, asegurando que su rendimiento fuera el mejor posible dentro de las opciones evaluadas.
-    """)
+    Una vez identificado el mejor conjunto de hiperparámetros, el modelo óptimo fue entrenado con la totalidad de los datos de entrenamiento y evaluado sobre el conjunto de prueba. Para medir su desempeño, se calcularon métricas clave como la **precisión (accuracy)**, además de visualizar su comportamiento mediante una **matriz de confusión y la curva ROC**.
+    
+    
     st.markdown("### Sube una imagen y el modelo la clasificará en una de las 10 categorías del dataset MNIST.")
     
     st.sidebar.header("Carga de Imagen")
