3e40eb0bce701917d862c74d3fd920bc22a0b695
Tbermejo <lbermejo@unal.edu.co>
2025-02-05 20:41:51 -0500

    Update aplicacion_mnist.py

--- aplicacion_mnist.py
+++ aplicacion_mnist.py
@@ -22,28 +22,53 @@ def load_model():
 
 # Función principal
 def main():
-    st.title("Clasificación de la base de datos MNIST")
-    st.markdown("Sube una imagen para clasificar")
+     st.set_page_config(page_title="Clasificación MNIST", layout="wide")
+    st.title("🖼️ Clasificación de imágenes MNIST")
+    st.write("""
+    ### Selección del Mejor Modelo Mediante Búsqueda de Hiperparámetros en diferentes métodos de clasificación.
 
-    uploaded_file = st.file_uploader("Selecciona una imagen (PNG, JPG, JPEG):", type=["jpg", "png", "jpeg"])
+    En este análisis, se implementó un proceso iterativo para encontrar el mejor modelo de clasificación utilizando el Clasificador Naive Bayes (GaussianNB)**, Árboles de decisión (DecisionTreeClassifier)**, Máquinas de Soporte Vectorial (SVC)**, entre otros. Para ello, se realizó una búsqueda de hiperparámetros mediante la técnica de **Grid Search con validación cruzada**, con el objetivo de optimizar la precisión del modelo y mejorar su capacidad de generalización.
 
+    Una vez identificado el mejor conjunto de hiperparámetros para el modelo: \n
+    **Datos de test:**  0.9175 \n
+    **Datos de train:**  0.9152 \n
+    **Mejor hiperparámetro:** {'C': 10, 'gamma': 'scale', 'kernel': 'rbf'} \n
+    **Mejor precisión:** 0.907 \n
+    **Kernel utilizado:** rbf \n
+    El modelo óptimo fue entrenado con la totalidad de los datos de entrenamiento y evaluado sobre el conjunto de prueba. Para medir su desempeño, se calcularon métricas clave como la **precisión (accuracy)**, además de visualizar su comportamiento mediante una **matriz de confusión y la curva ROC**.""")
+
+    # Cargar imágenes
+    img1 = Image.open("ACC_SVC.png").resize((300, 300))
+    img2 = Image.open("ROC curve SVC.png").resize((300, 300))
+
+    # Mostrar imágenes en dos columnas
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image(img1, caption="Matriz de confusión", use_container_width=True)
+    with col2:
+        st.image(img2, caption="Curva ROC", use_container_width=True)
+    
+    st.markdown("### Sube una imagen y el modelo la clasificará en una de las 10 categorías del dataset MNIST.")
+    st.write("""⬅️Ahora intenta clasificar tus imágenes en la barra lateral izquierda.""")
+    st.sidebar.header("Carga de Imagen")
+    uploaded_file = st.sidebar.file_uploader("Selecciona una imagen (PNG, JPG, JPEG):", type=["jpg", "png", "jpeg"])
+    
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption="Imagen subida", use_column_width=True)  # Mostrar imagen subida
-
-        # Preprocesar la imagen
+        
+        col1, col2 = st.columns(2)
+        with col1:
+            st.image(image, caption="Imagen original", use_container_width=True)
+        
         preprocessed_image = preprocess_image(image)
-
-        # Mostrar imagen preprocesada en la barra lateral
-        st.sidebar.image(preprocessed_image[0], caption="Imagen preprocesada", use_column_width=True)
-
-        if st.button("Clasificar imagen"):
-            st.markdown("Imagen clasificada")
+        
+        with col2:
+            st.image(image.convert('L').resize((28, 28)), caption="Imagen preprocesada", use_container_width=True)
+        
+        if st.sidebar.button("Clasificar imagen"):
             model = load_model()
-
-            # Realizar la predicción
-            prediction = model.predict(preprocessed_image.reshape(1, -1))  # Redimensionar para el modelo (1, 784)
-            st.markdown(f"La imagen fue clasificada como: {prediction[0]}")  # Mostrar la predicción
+            prediction = model.predict(preprocessed_image)
+            st.sidebar.success(f"🔢 La imagen fue clasificada como: '{prediction}'.")
 
 if __name__ == "__main__":
     main()
